42c8846c0d63f8004e0e957a2203d4b03d388275
Aseem Sood <asood123@yahoo.com>
2016-02-11 23:53:06 -0500

    disable donate button if there is no stripe key

--- frontend/src/components/PublicGroupForm.js
+++ frontend/src/components/PublicGroupForm.js
@@ -32,24 +32,25 @@ const PublicGroupForm = ({
         onChange={({amount, frequency}) => appendDonationForm({amount, frequency})} />
 
       <div className='PublicGroupForm-checkout'>
-      <StripeCheckout
-        token={onToken}
-        stripeKey={stripeKey}
-        name={group.name}
-        currency={group.currency}
-        amount={stripeAmount}
-        description={stripeDescription}>
-        <div className='PublicGroupForm-button'>
-          <AsyncButton
-            color='green'
-            inProgress={inProgress} >
-            Donate
-          </AsyncButton>
+      {stripeKey ?
+        (<StripeCheckout
+          token={onToken}
+          stripeKey={stripeKey}
+          name={group.name}
+          currency={group.currency}
+          amount={stripeAmount}
+          description={stripeDescription}>
+          <div className='PublicGroupForm-button'>
+            <AsyncButton
+              color='green'
+              inProgress={inProgress} >
+              Donate
+            </AsyncButton>
+          </div>
+        </StripeCheckout>) : (<AsyncButton color='gray' > Donate </AsyncButton>)}
+        <div className='PublicGroupForm-disclaimer'>
+          By clicking above, you are pledging to support us monthly. We appreciate it! And you can cancel anytime. <a href='https://opencollective.com/faq'>Learn more</a>
         </div>
-      </StripeCheckout>
-      <div className='PublicGroupForm-disclaimer'>
-        By clicking above, you are pledging to support us monthly. We appreciate it! And you can cancel anytime. <a href='https://opencollective.com/faq'>Learn more</a>
-      </div>
       </div>
     </div>
   );
