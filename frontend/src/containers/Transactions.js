773e11f679051f50eb04c19cf6523f39c749d247
Sebastien Dubois <sdubois@protonmail.com>
2016-02-23 13:36:49 +0200

    Display transactions on transactions page

--- frontend/src/containers/Transactions.js
+++ frontend/src/containers/Transactions.js
@@ -3,16 +3,29 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import values from 'lodash/object/values';
 
-import PublicTopBar from '../components/PublicTopBar';
-import PublicFooter from '../components/PublicFooter';
-import Currency from '../components/Currency';
+import filterCollection from '../lib/filter_collection';
 
+import Currency from '../components/Currency';
+import DisplayUrl from '../components/DisplayUrl';
+import Icon from '../components/Icon';
+import PublicFooter from '../components/PublicFooter';
+import PublicTopBar from '../components/PublicTopBar';
+import TransactionItem from '../components/TransactionItem';
+
+import fetchUsers from '../actions/users/fetch_by_group';
+import fetchTransactions from '../actions/transactions/fetch_by_group';
 import logout from '../actions/session/logout';
+import decodeJWT from '../actions/session/decode_jwt';
+
+// Number of expenses and revenue items to show on the transactions page
+const NUM_TRANSACTIONS_TO_SHOW = 4;
 
 export class Transactions extends Component {
   render() {
     const {
-      group
+      group,
+      transactions,
+      users
     } = this.props;
 
     return (
@@ -22,9 +35,14 @@ export class Transactions extends Component {
 
         <div className='PublicContent'>
           <div className='Widget-header'>
-            <img src={group.logo} />
-            <h1>{group.name}</h1>
-            <p>{group.description}</p>
+
+            <div className='PublicGroupHeader'>
+              <img className='PublicGroupHeader-logo' src={group.logo ? group.logo : '/static/images/media-placeholder.svg'} />
+              <div className='PublicGroupHeader-website'><DisplayUrl url={group.website} /></div>
+              <div className='PublicGroupHeader-description'>
+                {group.description}
+              </div>
+            </div>
 
             <div className='Widget-balance'>
               <Currency
@@ -33,6 +51,24 @@ export class Transactions extends Component {
             </div>
             <div className='Widget-label'>Available funds</div>
           </div>
+
+          <div className='PublicGroup-expenses'>
+            <h2>Latest transactions</h2>
+            {(transactions.length === 0) && (
+              <div className='PublicGroup-emptyState'>
+                <div className='PublicGroup-expenseIcon'>
+                  <Icon type='expense' />
+                </div>
+                <label>
+                  All transactions will show up here
+                </label>
+              </div>
+            )}
+            {transactions.map(tx => <TransactionItem
+                                       key={tx.id}
+                                       transaction={tx}
+                                       user={users[tx.UserId]} />)}
+          </div>
         </div>
         <PublicFooter />
       </div>
@@ -40,22 +76,46 @@ export class Transactions extends Component {
   }
 
   componentWillMount() {
+    const {
+      group,
+      fetchTransactions,
+      fetchUsers
+    } = this.props;
 
+    fetchTransactions(group.id, {
+      per_page: NUM_TRANSACTIONS_TO_SHOW,
+      sort: 'updatedAt',
+      direction: 'desc'
+    });
+
+    fetchUsers(group.id);
+  }
+
+  componentDidMount() {
+    // decode here because we don't handle auth on the server side yet
+    this.props.decodeJWT();
   }
 }
 
 export default connect(mapStateToProps, {
-  logout
+  fetchTransactions,
+  fetchUsers,
+  logout,
+  decodeJWT
 })(Transactions);
 
 function mapStateToProps({
   session,
-  groups
+  groups,
+  transactions,
+  users
 }) {
   const group = values(groups)[0] || {}; // to refactor to allow only one group
 
   return {
     session,
-    group
+    group,
+    transactions: values(transactions),
+    users
   };
 }
