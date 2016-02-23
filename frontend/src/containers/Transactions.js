fddd14160f9a8e50cd4f1a79ad90c471fd07499f
Sebastien Dubois <sdubois@protonmail.com>
2016-02-23 14:56:28 +0200

    Improve CSS just a bit

--- frontend/src/containers/Transactions.js
+++ frontend/src/containers/Transactions.js
@@ -52,7 +52,7 @@ export class Transactions extends Component {
             <div className='Widget-label'>Available funds</div>
           </div>
 
-          <div className='PublicGroup-expenses'>
+          <div className='PublicGroup-transactions'>
             <h2>Latest transactions</h2>
             {(transactions.length === 0) && (
               <div className='PublicGroup-emptyState'>
