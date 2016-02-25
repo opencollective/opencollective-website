d35fb17ca519515857d3d5d2896895976aa4641a
Sebastien Dubois <sdubois@protonmail.com>
2016-02-25 23:10:22 +0200

    Configurability for running selenium tests with local test_server API

--- test/frontend/public_donation_page.js
+++ test/frontend/public_donation_page.js
@@ -1,12 +1,14 @@
+const config = require('config');
+
 module.exports = {
   '@tags': ['donation'],
   beforeEach: (client) => {
     client
 
       // reset test database
-      .url('https://opencollective-test-api.herokuapp.com/database/reset')
+      .url(config.host.testserver.api + '/database/reset')
 
-      .url('http://localhost:3000/testcollective')
+      .url(config.host.website + '/testcollective')
       .waitForElementVisible('body', 1000)
       .assert.containsText('body', 'OpenCollective test group on the test server')
       .waitForElementVisible('div[class=Tiers]', 1000)
