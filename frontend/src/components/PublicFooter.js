e93e8ac4c59c1e91794696d959186a8f1cb125df
Noel Delgado <auhcsei@gmail.com>
2016-03-22 12:20:22 -0600

    update / PublicFooter

--- frontend/src/components/PublicFooter.js
+++ frontend/src/components/PublicFooter.js
@@ -2,11 +2,37 @@ import React from 'react';
 
 export default () => {
   return  (
-    <div className='PublicFooter'>
-      <img src='/static/images/logo-grey.svg' />
-      <div className='PublicFooter-menu'>
-        <div className='PublicFooter-menuItem'>FAQ</div>
-        <div className='PublicFooter-menuItem'>Contact</div>
+    <div className='PublicFooter px2'>
+      <div className='container clearfix'>
+        <div className='left pt3'>
+          <svg width='14' height='14' className='inline-block align-middle mr1'>
+            <use xlinkHref='#svg-isotype' fill='#7FADF2'/>
+          </svg>
+          <svg width='138' height='20' className='inline-block align-middle'>
+            <use xlinkHref='#svg-logotype' fill='#919699'/>
+          </svg>
+        </div>
+        <div className='PublicFooter-menu right'>
+          <nav>
+            <ul className='list-reset m0'>
+              <li className='inline-block'>
+                <a className='block h6 py3 px2 white muted -ff-sec -fw-bold' href='https://opencollective.com/#how'>How It Works</a>
+              </li>
+              <li className='inline-block'>
+                <a className='block h6 py3 px2 white muted -ff-sec -fw-bold' href='https://opencollective.com/#usecases'>Use Cases</a>
+              </li>
+              <li className='inline-block'>
+                <a className='block h6 py3 px2 white muted -ff-sec -fw-bold' href='https://opencollective.com/faq'>FAQ</a>
+              </li>
+              <li className='inline-block'>
+                <a className='block h6 py3 px2 white muted -ff-sec -fw-bold' href='https://opencollective.com/about'>About</a>
+              </li>
+              <li className='inline-block'>
+                <a className='block h6 py3 px2 white muted -ff-sec -fw-bold' href='mailto:info@opencollective.com'>Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </div>
   );
