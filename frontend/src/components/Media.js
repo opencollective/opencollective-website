6ef2cf1324e0e408d0599eb2242f08b314968e69
Noel Delgado <auhcsei@gmail.com>
2016-03-22 12:31:21 -0600

    update / PublicGroup Media styles

--- frontend/src/components/Media.js
+++ frontend/src/components/Media.js
@@ -5,16 +5,16 @@ import YoutubeVideo from './YoutubeVideo';
 export default ({group}) => {
   if (group.video) {
     return (
-      <div className='PublicGroup-video'>
+      <div className='PublicGroup-video absolute width-100 height-100'>
         <YoutubeVideo video={group.video} />
       </div>
     );
   } else if (group.image) {
-    return (
-      <div className='PublicGroup-image'>
-        <img src={`https://res.cloudinary.com/opencollective/image/fetch/w_720/${encodeURIComponent(group.image)}`} />
-      </div>
-    );
+    const image = `https://res.cloudinary.com/opencollective/image/fetch/w_720/${encodeURIComponent(group.image)}`;
+    const styles = {
+      backgroundImage: `url(${image})`,
+    }
+    return (<div className='PublicGroup-image absolute bg-contain bg-center bg-no-repeat height-100 width-100' style={styles}></div>);
   } else {
     return (
       <div className='PublicGroup-image'>
