5e734861be7ea77a405b56a2c938082f7ccf2c8b
Noel Delgado <auhcsei@gmail.com>
2016-03-30 12:49:39 -0600

    update small / Metrics spacing

--- frontend/src/components/Metric.js
+++ frontend/src/components/Metric.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default ({ className = '', label, value, children }) => (
   <div className={`Metric ${className}`}>
-    <div className='Metric-value h3 -ff-sec -fw-bold mb1'>
+    <div className='Metric-value h3 -ff-sec -fw-bold'>
       {children || value}
     </div>
     <div className='Metric-label h6 muted'>{label}</div>
