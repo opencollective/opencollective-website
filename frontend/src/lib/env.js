80dae7732564c738166ed88bc0a038fe04ee53c5
Sebastien Dubois <sdubois@protonmail.com>
2016-04-07 22:49:19 +0300

    Fix npm start in development env, wasn't connecting to API (npm run dev was OK)

--- frontend/src/lib/env.js
+++ frontend/src/lib/env.js
@@ -2,12 +2,20 @@
  * Environment variables
  */
 
+if (typeof process.env.NODE_ENV === 'undefined') {
+  process.env.NODE_ENV = 'development';
+}
+
 const env = {
   NODE_ENV: process.env.NODE_ENV,
   API_ROOT: `${process.env.WEBSITE_URL}/api`
 };
 
-if (env.NODE_ENV === 'test' || env.NODE_ENV === 'development') {
+// TODO NODE_ENV=test shouldn't be needed once NODE_ENV harmonized to circleci on circleci
+if (
+  env.NODE_ENV === 'circleci' ||
+  env.NODE_ENV === 'test' ||
+  env.NODE_ENV === 'development') {
   env.API_ROOT = 'http://localhost:3000/api';
 }
 
