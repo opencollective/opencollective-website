3487a4bfbe01ceb5755b2c055e25e3bbb2bc0e94
Sebastien Dubois <sdubois@protonmail.com>
2016-04-08 17:29:43 +0300

    Avoid apt-get update altogether when deb available

--- scripts/install_apt_get_debs.sh
+++ scripts/install_apt_get_debs.sh
@@ -1,12 +1,33 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# cfr. https://discuss.circleci.com/t/add-ability-to-cache-apt-get-programs/598/6
 
 set -e
 
 APT_PACKAGES=(google-chrome-stable)
 APT_CACHE=~/cache/apt
 
-mkdir -p ${APT_CACHE}/partial
+# Work from the directory CI will cache
+mkdir -p ${APT_CACHE}
+cd ${APT_CACHE}
+
+# check we have a deb for each package
+useCache=true
+for pkg in "${APT_PACKAGES[@]}"; do
+  if ! ls | grep "^${pkg}"; then
+    useCache=false
+  fi
+done
+
+set -x
+
+if [ ${useCache} == true ]; then
+  sudo dpkg -i *.deb
+  exit 0
+fi
+
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get -o dir::cache::archives=${APT_CACHE} update
-sudo apt-get -o dir::cache::archives=${APT_CACHE} install -y "${APT_PACKAGES[@]}"
+sudo apt-get update
+sudo apt-get install --reinstall "${APT_PACKAGES[@]}"
+
+cp -v /var/cache/apt/archives/*.deb ${APT_CACHE}
