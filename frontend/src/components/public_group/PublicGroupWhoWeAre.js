460a236d0ffdfb942a70239b266c543ab387bdc1
Noel Delgado <auhcsei@gmail.com>
2016-04-13 15:36:28 -0500

    add / no logo placeholder

--- frontend/src/components/public_group/PublicGroupWhoWeAre.js
+++ frontend/src/components/public_group/PublicGroupWhoWeAre.js
@@ -7,13 +7,19 @@ import { displayUrl } from '../../components/DisplayUrl';
 export default class PublicGroupWhoWeAre extends React.Component {
   render() {
     const { group } = this.props;
+    let logoElement = null;
+    let noLogoClassName = '';
+
+    if (group.logo) {
+      logoElement = <img className='PublicGroupIntro-logo mb2 rounded' src={group.logo} />;
+    } else {
+      noLogoClassName = 'PublicGroupIntro--no-logo bg-no-repeat';
+    }
 
     return (
       <section id='who-we-are' className='PublicGroupIntro px2 bg-light-gray relative'>
-        <div className='PublicGroupIntro-container container center relative'>
-          {group.logo && (
-            <img className='PublicGroupIntro-logo mb2 rounded' src={group.logo} />
-          )}
+        <div className={`PublicGroupIntro-container container center relative ${noLogoClassName}`}>
+          {logoElement}
           <h2 className='PublicGroupIntro-title m0 -ff-sec -fw-bold'>We are {group.name}</h2>
           <h3 className='PublicGroup-subtitle mt0 mb2 -ff-sec -fw-light'>{group.description}</h3>
           <div className='PublicGroup-font-15 PublicGroup-quote max-width-3 mx-auto'>
@@ -22,11 +28,11 @@ export default class PublicGroupWhoWeAre extends React.Component {
             )}
           </div>
 
-          {group.website ? (
+          {group.website && (
             <div className='PublicGroupIntro-website pt3'>
               <a href={group.website} className='px3 -btn -green -btn-outline -btn-small -ttu -ff-sec -fw-bold'>{displayUrl(group.website)}</a>
             </div>
-          ) : null}
+          )}
 
           {group.members.length ? (
             <div className='PublicGroup-members pt4'>
