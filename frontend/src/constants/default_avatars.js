d46b12f082addaf1c0d5ad6fbcab86f0eae37ff6
Noel Delgado <auhcsei@gmail.com>
2016-04-14 16:19:14 -0500

    add / constants: default avatars list

---
+++ frontend/src/constants/default_avatars.js
@@ -0,0 +1,6 @@
+module.exports = [
+  '/static/images/users/avatar-01.svg',
+  '/static/images/users/avatar-02.svg',
+  '/static/images/users/avatar-03.svg',
+  '/static/images/users/avatar-04.svg'
+];
