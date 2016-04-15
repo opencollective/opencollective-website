a317be0a564bc36d4e04fa512673ea0eaeed68a4
Xavier Damman <xdamman@gmail.com>
2016-04-15 18:29:31 -0400

    disclaimer in French

--- frontend/src/components/public_group/AmountPicker.js
+++ frontend/src/components/public_group/AmountPicker.js
@@ -13,6 +13,7 @@ export default class AmountPicker extends React.Component {
       onToken,
       appendDonationForm,
       tier,
+      i18n,
       inProgress
     } = this.props;
 
@@ -23,10 +24,10 @@ export default class AmountPicker extends React.Component {
     const currency = donationForm[tier.name].currency || group.currency;
     const hasPaypal = group.hasPaypal;
     const hasStripe = stripeKey && amount !== '';
-    const frequencyHuman = frequency === 'one-time' ? '' : `per ${frequency.replace(/ly$/,'')}`;
+    const frequencyHuman = frequency === 'one-time' ? '' : `${i18n.getString('per')} ${i18n.getString(frequency.replace(/ly$/,''))}`;
     const stripeDescription =  `${formatCurrency(amount, currency, { compact: false })} ${frequencyHuman}`;
     const button = tier.button || `Donate ${stripeDescription}`;
-    const cancellationDisclaimer = (frequency !== 'one-time') ? "You can cancel anytime." : "";
+    const cancellationDisclaimer = (frequency !== 'one-time') ? i18n.getString('cancelAnytime') : "";
 
     return (
       <div>
@@ -77,7 +78,7 @@ export default class AmountPicker extends React.Component {
           )}
 
           <div className='PublicGroupForm-disclaimer h6 mt2' style={{minHeight: 38}}>
-            By clicking above, you are pledging to give {group.host.name} {stripeDescription} for {group.name}. {cancellationDisclaimer}
+            {i18n.getString('disclaimer')} {group.host.name} {stripeDescription} {i18n.getString('for')} {group.name}. {cancellationDisclaimer}
           </div>
         </div>
       </div>
