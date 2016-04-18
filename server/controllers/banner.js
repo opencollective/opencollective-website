820b4660f31558e81fb5dfea14d16c4cbcdd6cde
Xavier Damman <xdamman@gmail.com>
2016-04-18 13:54:37 -0700

    making sure we only show a user once as a backer even if already an admin (#168)

--- server/controllers/banner.js
+++ server/controllers/banner.js
@@ -1,9 +1,10 @@
 import config from 'config';
 import request from 'request';
 import filterCollection from '../../frontend/src/lib/filter_collection';
+import _ from 'lodash';
 
 const filterUsersByTier = (users, tiername) => {
-  return filterCollection(users, { tier: tiername });
+  return _.uniq(filterCollection(users, { tier: tiername }), 'id');
 }
 
 module.exports = {
@@ -44,7 +45,6 @@ module.exports = {
     const tierSingular = tier.replace(/s$/,'');
     const users = filterUsersByTier(req.users, tierSingular);
     const position = parseInt(req.params.position, 10);
-        
     const user = (position < users.length) ?  users[position] : {};
 
     // We only record a page view when loading the first avatar
