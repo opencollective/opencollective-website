5b9a0043db6cbcf6f666cf35344a084478f5d7d0
Ascari <carlos.ascari.x@gmail.com>
2016-05-12 18:53:08 -0500

    Updates for On Boarding Container and related components ImagePicker was tweaked to allow custom labeling and image preview size Added Checkbox general purpose component Added CustomTextArea general purpose component, includes max-length counter and eases styling of placeholder by not realing on pseudo selectors, (the placeholder is its own div)

---
+++ frontend/src/components/CustomTextArea.js
@@ -0,0 +1,70 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class CustomTextArea extends Component {
+
+  static propTypes = {
+    className: PropTypes.string,
+    cols: PropTypes.number,
+    disabled: PropTypes.bool,
+    maxLength: PropTypes.number,
+    onChange: PropTypes.func,
+    placeholder: PropTypes.string,
+    resize: PropTypes.oneOf(['none', 'both', 'veritcal', 'horizontal']),
+    rows: PropTypes.number,
+    value: PropTypes.string
+  }
+
+  static defaultProps = {
+    className: '',
+    disabled: false,
+    placeholder: '',
+    resize: 'none',
+    value: ''
+  }
+
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      hasScrollbar: false
+    }
+  }
+  
+  render()
+  {
+    const { disabled, onChange, className, placeholder, value, maxLength, rows, cols, resize } = this.props;
+    const { hasScrollbar } = this.state;
+    return (
+      <div className={`CustomTextArea ${className} ${disabled ? 'CustomTextArea--disabled' : ''} ${hasScrollbar ? 'CustomTextArea-has-scrollbar' : ''}`}>
+      {!value && <div className="CustomTextArea-placeholder" onClick={this.onPlaceholderClick.bind(this)}>{placeholder}</div>}
+      <textarea
+        ref='textarea'
+        className='CustomTextArea-textarea'
+        maxLength={maxLength}
+        value={value}
+        rows={rows}
+        cols={cols}
+        style={{
+          width: cols ? 'auto' : null,
+          height: rows ? 'auto' : null,
+          resize: resize
+        }}
+        onChange={(e) => onChange(e.target.value) }
+        onKeyUp={this.detectScrollbar.bind(this)}
+      />
+      {maxLength && <div className='CustomTextArea-counter'>{maxLength - value.length}</div>}
+      </div>
+    )
+  }
+
+  onPlaceholderClick()
+  {
+    this.refs.textarea.focus();
+  }
+
+  detectScrollbar()
+  {
+    const element = this.refs.textarea;
+    this.setState({hasScrollbar: element.clientHeight < element.scrollHeight})
+  }
+}
--- frontend/src/components/on_boarding/OnBoardingStepCreate.js
+++ frontend/src/components/on_boarding/OnBoardingStepCreate.js
@@ -1,22 +1,50 @@
 import React from 'react';
 
 import OnBoardingStepHeading from './OnBoardingStepHeading';
+import ImagePicker from '../ImagePicker';
+import CustomTextArea from '../CustomTextArea';
+import Checkbox from '../Checkbox';
 
 export default class OnBoardingStepCreate extends React.Component {
 
   constructor(props)
   {
     super(props);
+    this.state = {
+      agreedTOS: false,
+      expenseDescription: '',
+      missionDescription: ''
+    }
   }
   
   render()
   {
     const { onCreate } = this.props;
+    const { agreedTOS, expenseDescription, missionDescription } = this.state;
     return (
       <div className="OnBoardingStepCreate">
         <OnBoardingStepHeading step="4/4" title="Why do you want to create a collective?" subtitle="The answers will be public and help you motivate people to back your project."/>
+        <div className="OnBoardingStepCreate-form-container">
+          <div className="OnBoardingStepCreate-label">Help us on our mission to...</div>
+          <div className="flex">
+            <div className="flex-auto">
+              <div className="flex flex-column">
+                <CustomTextArea value={missionDescription} onChange={(value) => this.setState({missionDescription: value})} maxLength={100} placeholder="State the collective core mission"/>
+                <div className="OnBoardingStepCreate-label" style={{marginTop: '25px'}}>How are you going to spend the funds?</div>
+                <CustomTextArea value={expenseDescription} onChange={(value) => this.setState({expenseDescription: value})} maxLength={100} placeholder="Development, design, host, etc…"/>
+              </div>
+            </div>
+            <div>
+              <ImagePicker className="logo" label="Select Collective Image" dontLookupSocialMediaAvatars handleChange={Function.prototype}/>
+            </div>
+          </div>
+          <div className="OnBoardingStepCreate-tos">
+            <Checkbox checked={agreedTOS} onChange={(checked) => this.setState({agreedTOS: checked})} />
+            <span>Agree to <a href="#TOS">Terms &amp; Conditions</a></span>
+          </div>
+        </div>
         <div style={{margin: '0 auto', marginTop: '40px', width: '300px', textAlign: 'center'}}>
-          <a href="#" className="OnBoardingStepButton mb3 -btn -btn-big -bg-green -ttu -ff-sec -fw-bold color-white m-auto" onClick={onCreate}>create!</a>
+          <div className='OnBoardingButton' onClick={onCreate}>create!</div>
         </div>
       </div>
     )
