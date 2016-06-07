c7684ea51e5a73d8b6648ed4a255986a20c436f4
Ascari <carlos.ascari.x@gmail.com>
2016-06-07 18:05:18 -0500

    Completed scaffolding Modifed UserPhoto bugfix to do image sanity test inside of componentDidMount instead of contrucutor Expectings group.settings.pending to be true in order to activate, right now its hard coded since backend is need for true completion Uses realistic mocked data

--- frontend/src/components/public_group/BackerCard.js
+++ frontend/src/components/public_group/BackerCard.js
@@ -6,6 +6,6 @@ export default ({user, title, onClick}) => (
   <div className='BackerCard'>
   	<UserPhoto user={user} addBadge={true} customBadge='svg-star-badge' customBadgeSize='20' />
   	<div className='BackerCard-title'>{title}</div>
-  	{onClick && <div className="BackerCard-button">become backer</div>}
+  	{onClick && <div className="BackerCard-button" onClick={onClick}>become backer</div>}
   </div>
 );
