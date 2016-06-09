469a8c2c1659c15fe0b2eaf51d68942d95a098bc
Aseem Sood <asood123@yahoo.com>
2016-06-09 17:54:19 -0400

    more test fixes

--- test/e2e/github_apply.js
+++ test/e2e/github_apply.js
@@ -31,14 +31,17 @@ module.exports = {
       .waitForElementVisible('body', 10000)
       // TODO this returns OK even if it's a 404 page: 404s should change window location
       .assert.urlContains('http://localhost:3000/github/apply')
+      .pause(1000)
       .click('.OnBoardingButton') // Repo already selected, click continue
       .pause(500)
       .click('.ContributorPickerItemSearch-list-container') // pick a collaborator
+      .pause(500)
       .click('.OnBoardingButton')
       .pause(500)
       .setValue('textarea[name=mission]', 'fund open source projects')
       .setValue('textarea[name=expensePolicy]', 'pay for servers and tshirts')
       .click('.Checkbox')
+      .pause(500)
       .click('.OnBoardingButton')
       .pause(500)
       .assert.containsText('.OnBoardingButton', 'EXPLORE EXISTING COLLECTIVES')
