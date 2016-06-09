76178452309fe9c1bac1b3dfda0a8cd1b41fde15
asood123 <asood123@users.noreply.github.com>
2016-06-09 16:57:28 -0400

    Revert "Pending collective"

--- frontend/src/containers/PublicGroup.js
+++ frontend/src/containers/PublicGroup.js
@@ -10,9 +10,6 @@ import filterCollection from '../lib/filter_collection';
 import roles from '../constants/roles';
 import Notification from '../containers/Notification';
 import PublicFooter from '../components/PublicFooter';
-import UserCard from '../components/UserCard';
-
-import OnBoardingHeader from '../components/on_boarding/OnBoardingHeader';
 
 import PublicGroupHero from '../components/public_group/PublicGroupHero';
 import PublicGroupWhoWeAre from '../components/public_group/PublicGroupWhoWeAre';
@@ -23,8 +20,6 @@ import PublicGroupExpenses from '../components/public_group/PublicGroupExpenses'
 import PublicGroupDonations from '../components/public_group/PublicGroupDonations';
 import PublicGroupSignupV2 from '../components/public_group/PublicGroupSignupV2';
 import PublicGroupThanksV2 from '../components/public_group/PublicGroupThanksV2';
-import BackerCard from '../components/public_group/BackerCard';
-import ContributorList from '../components/public_group/ContributorList';
 
 import fetchGroup from '../actions/groups/fetch_by_id';
 import fetchUsers from '../actions/users/fetch_by_group';
@@ -92,115 +87,6 @@ export class PublicGroup extends Component {
     });
   }
 
-  renderPendingBackers() {
-    const backers = [ // TODO: replace with current backers of pending collective
-      { name: 'Ascari', tier: 'backer', avatar: 'https://avatars2.githubusercontent.com/u/2263170?s=96' }
-    ];
-    const backersCount = backers.length;
-  
-    if (backers.length < 10) {
-      for (var i = 0, delta = 10 - backers.length; i < delta; i++) {
-        backers.push(null)
-      }
-    }
-
-    return (
-      <div className="PublicGroup-backer-container">
-        <div className="-top-gradient"></div>
-        <div className="-wrap">
-          {backers.map((backer, index) => {
-            if (backer) {
-              return <UserCard user={backer} {...this.props}/>
-            } else {
-              return (
-                <BackerCard 
-                  key={index}
-                  title={`${index+1}st Backer`} 
-                  user={{avatar: ''}}
-                  onClick={index === (backersCount) ? () => { /* TODO Add a backer flow starts here */ } : null}
-                />
-              )
-            }
-          })}
-        </div>
-        <div className="mb4">
-          <small style={{color: '#919699'}}>You won’t be charged a single penny until we reach our 10 backer goal. <a href="#">More info</a></small>
-        </div>
-        <div className="-bottom-gradient"></div>
-      </div>
-    )
-  }
-
-  renderPendingContributors() {
-    const contributors = [ // TODO replace with contributors of opensource project
-      {name: 'Xavier Damman', avatar: 'https://avatars.githubusercontent.com/xdamman?s=96', core: true},
-      {name: 'Pia Mancini', avatar: 'https://avatars.githubusercontent.com/piamancini?s=96', core: true},
-      {name: 'Sébastien Dubois', avatar: 'https://avatars.githubusercontent.com/sedubois?s=96', core: true},
-      {name: 'Kristaps Ledins', avatar: 'https://avatars.githubusercontent.com/krysits?s=96'},
-      {name: 'Ericku', avatar: 'https://avatars.githubusercontent.com/erickrico?s=96'},
-      {name: 'Javier Bórquez', avatar: 'https://avatars.githubusercontent.com/javierbyte?s=96'},
-      {name: 'Alvaro Cabrera Durán', avatar: 'https://avatars.githubusercontent.com/pateketrueke?s=96'},
-      {name: 'Eduardo Lavaque', avatar: 'https://avatars.githubusercontent.com/greduan?s=96'},
-      {name: 'dvaughan', avatar: 'https://avatars.githubusercontent.com/dsvaughan?s=96'},
-      {name: 'Sergio de la Garza', avatar: 'https://avatars.githubusercontent.com/sgarza?s=96'},
-      {name: 'Michael Anthony', avatar: 'https://avatars.githubusercontent.com/mcanthony?s=96'},
-      {name: 'José Moreira', avatar: 'https://avatars.githubusercontent.com/cusspvz?s=96'},
-      {name: 'eduardoalbertorg', avatar: 'https://avatars.githubusercontent.com/eduardoalbertorg?s=96'},
-      {name: 'alfredopizana', avatar: 'https://avatars.githubusercontent.com/alfredopizana?s=96'},
-      {name: 'Javier Murillo', avatar: 'https://avatars.githubusercontent.com/javiermurillo?s=96'},
-      {name: 'Linnk Benavides', avatar: 'https://avatars.githubusercontent.com/Linnk?s=96'},
-      {name: 'Adrián Zúñiga Espinoza', avatar: 'https://avatars.githubusercontent.com/Adrian0350?s=96'},
-      {name: 'Ascari', avatar: 'https://avatars.githubusercontent.com/carlosascari?s=96'},
-    ].map((contributor) => {
-      contributor.stats = {
-        c: ~~(Math.random() * 400),
-        a: ~~(Math.random() * 4000),
-        d: ~~(Math.random() * 999)
-      };
-      return contributor;
-    });
-    return (
-      <div className="PublicGroup-contrib-container">
-        <div className="line1" >Contributors</div>
-        <ContributorList contributors={contributors} />
-      </div>
-    )
-  }
-
-  renderPendingAbout() {
-    return (
-      <div className="PublicGroup-about-container">
-        <div className="line1">About Open Collective</div>
-        <div className="line2">
-          We use [Open Collective host] to collect the funds on our behalf using OpenCollective. Whenever we need to use the money for something, we will submit the invoice or expense via the OpenCollective app and once approved we will be reimbursed. That way, you can always track our budget. 
-          <br/>
-          <b>Everything is transparent.</b>
-        </div>
-        <div className="more-button">learn more</div>
-      </div>
-    ) 
-  }
-
-  renderPending() {
-    const { group } = this.props;
-    return (
-      <div className='PublicGroup PublicGroup--inactive'>
-        <OnBoardingHeader />
-        <div className="PublicGroupHero-logo mb3 bg-contain" style={{backgroundImage: `url(${'https://cldup.com/U1yzUnB9YJ.png'})`}} ></div>
-        <div className="line1">Help <a href={group.website}>{group.name}</a> create an open collective to…</div>
-        <div className="line2">{group.mission}</div>
-        <div className="line3">Help us get the first 10 backers to start the collective going.</div>
-        <div className="line4">With at least $10 you can become a member and help us cover design work, maintenance and servers.</div>
-        {this.renderPendingBackers()}
-        <div className="line5">Thank you for your visit</div>
-        <div className="line6">We are the contributors of this collective nice to meet you.</div>
-        {this.renderPendingContributors()}
-        {this.renderPendingAbout()}
-        <PublicFooter />
-      </div>
-    )
-  }
-
   render() {
     const {
       group,
@@ -209,10 +95,6 @@ export class PublicGroup extends Component {
       users,
       // shareUrl,
     } = this.props;
-
-    if (group.settings.pending) {
-      return this.renderPending();
-    }
 
     const publicGroupClassName = `PublicGroup ${group.slug}`;
 
