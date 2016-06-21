1190c266e69f2152de2f44cfc5b6106dbecc6c39
Ascari <carlos.ascari.x@gmail.com>
2016-06-21 17:21:21 -0500

    Added mocha unit test

---
+++ test/unit/components/OnBoardingStepCreate.js
@@ -0,0 +1,30 @@
+import { expect } from 'chai';
+import TestUtils from 'react-addons-test-utils';
+import OnBoardingStepCreate from '../../../frontend/src/components/on_boarding/OnBoardingStepCreate';
+
+describe('OnBoardingStepCreate component', () => {
+  it('Create button should disable for 5 seconds after being clicked', function(done) {
+    this.timeout(6000)
+    var callCount = 0
+    const element = TestUtils.renderIntoDocument(
+      <OnBoardingStepCreate onCreate={() => callCount++} githubForm={{attributes: {}}} uploadImage={Function.prototype}/>
+    );
+    expect(element.state.disableCreateButton).to.be.false;
+    element.onCreate();
+    expect(element.state.disableCreateButton).to.be.true;
+    for (var i = Math.floor(2 + Math.random() * 10); i >= 0; i--) element.onCreate();
+    expect(element.state.disableCreateButton).to.be.true;
+    setTimeout(() => {
+        for (var i = Math.floor(2 + Math.random() * 2); i >= 0; i--) element.onCreate();
+        expect(element.state.disableCreateButton).to.be.true;
+        setTimeout(() => {
+            expect(element.state.disableCreateButton).to.be.false;
+            expect(callCount).to.be.equal(1);
+            for (var i = Math.floor(2 + Math.random() * 3); i >= 0; i--) element.onCreate();
+            expect(callCount).to.be.equal(2);
+            expect(element.state.disableCreateButton).to.be.true;
+            done()
+        }, 4020)
+    }, 1000)
+  });
+});
