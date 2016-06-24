dd3c90d6a55dbed8fc4b59e8929921fd69cbdd83
Xavier Damman <xdamman@gmail.com>
2016-06-24 17:03:36 -0400

    making tests great again

--- frontend/src/components/ProfilePhoto.js
+++ frontend/src/components/ProfilePhoto.js
@@ -2,8 +2,7 @@ import React, { PropTypes } from 'react';
 
 const ProfilePhoto = ({url, spinner, hasBorder=false}) => {
 
-  const httpsImageURL = `https://res.cloudinary.com/opencollective/image/fetch/h_128/${encodeURIComponent(url)}`;
-  const backgroundImage = httpsImageURL || '/static/images/default_avatar.svg';
+  const backgroundImage = url ? `https://res.cloudinary.com/opencollective/image/fetch/h_128/${encodeURIComponent(url)}` : '/static/images/default_avatar.svg';
 
   const border = hasBorder ? {
     border: '3px solid white',
