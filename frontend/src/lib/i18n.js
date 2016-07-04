99d2218a722b6bed9b3dc9efd7fda25982ad2d33
Xavier Damman <xdamman@gmail.com>
2016-07-04 18:33:49 +0200

    FR and ES for submittedBy

--- frontend/src/lib/i18n.js
+++ frontend/src/lib/i18n.js
@@ -4,8 +4,6 @@ import strings from '../ui/strings.json';
 module.exports = (lang) => {
   lang = lang || 'en';
   moment.locale(lang);
-  console.log("Setting moment lang to ", lang);
-  console.log(moment.locales());
   return {
     getString: (strid) => {
       return strings[lang][strid] || strings['en'][strid];
