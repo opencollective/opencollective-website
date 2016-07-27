9e38f526140e9c78e5d3a38f60d95506c10dbf12
Ascari <carlos.ascari.x@gmail.com>
2016-07-26 19:09:33 -0500

    Restructured Subscriptions container, began changing style

--- frontend/src/containers/Subscriptions.js
+++ frontend/src/containers/Subscriptions.js
@@ -10,6 +10,7 @@ import TransactionItem from '../components/TransactionItem';
 import Notification from '../containers/Notification';
 import InlineToggle from '../components/InlineToggle';
 import ProfilePhoto from '../components/ProfilePhoto';
+import SubscriptionItem from '../components/SubscriptionItem';
 
 import cancelSubscription from '../actions/subscriptions/cancel';
 import notify from '../actions/notification/notify';
@@ -26,81 +27,13 @@ export class Subscriptions extends Component {
     } = this.props;
 
     return (
-      <div className='Subscription'>
-        <div className='container center'>
+      <div className='Subscriptions'>
         <LoginTopBar />
-
         <Notification />
-        <div className='PublicContent'>
-          <div className='Subscription-header'>
-            <div className='Subscription-page-title'>
-              <h3> Your Subscriptions </h3>
-            </div>
-            {subscriptions.length > 0 &&
-              <div className='Subscription-user'>
-                 <ProfilePhoto
-                    hasBorder={true}
-                    url={subscriptions[0].Transactions[0].User && subscriptions[0].Transactions[0].User.avatar} />
-                {subscriptions[0].Transactions[0].User.name ?
-                  <span className='Subscription-user-name' title={subscriptions[0].Transactions[0].User.email}> {subscriptions[0].Transactions[0].User.name}</span> :
-                  <span className='Subscription-user-name'> {subscriptions[0].Transactions[0].User.email}</span>
-                }
-              </div>
-            }
-          </div>
-          {subscriptions.map(subscription => {
-              const divStyle = {
-                backgroundImage: `url(${subscription.Transactions[0].Group.logo})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'contain',
-                backgroundPosition: 'center'
-              };
-            return (
-                <div className='Subscription-group' key={subscription.id} >
-                  <div className='Subscription-logo' style={divStyle} onClick={() => pushState(null, `/${subscription.Transactions[0].Group.slug}`)} >
-                  </div>
-                  <div className='Subscription-info'>
-                    <div className='Subscription-name'>
-                        {subscription.Transactions[0].Group.name}
-                    </div>
-                    <div className='Subscription-amount'>
-                        <Currency value={subscription.amount} currency={subscription.currency} colorify={false}/> {subscription.interval}ly
-                        {subscription.isActive &&
-                        subscription.createdAt ?
-                          <span className='Subscription-status'>
-                            &nbsp;({i18n.getString('since')} {moment(subscription.createdAt).format('MMM YYYY')})
-                            {subscription.isActive &&
-                              <span className='Subscription-cancel' onClick={cancel.bind(this, subscription.id)}>CANCEL</span>}
-                          </span> :
-                        <span className='Subscription-status'> (Inactive) </span>
-                      }
-                    </div>
-                  </div>
-                    <div className='Subscription-transactions'>
-                      <InlineToggle showString='Show transactions' hideString='Hide transactions'>
-                        {subscription.Transactions.map(transaction => {
-                          return <TransactionItem key={transaction.id} transaction={transaction} i18n={i18n} />;
-                        })}
-                      </InlineToggle>
-                    </div>
-
-                </div>
-              );
-          })}
-          {subscriptions.length > 0 &&
-            <div className='Subscription-text Subscription-bottom-text'>
-              <p> Visit our <a href='https://www.opencollective.com/leaderboard'>Leaderboard</a> to find more collectives to support! </p>
-            </div>
-          }
-          {subscriptions.length === 0 &&
-            <div className='Subscription-text'>
-            <p> Looks like you aren't contributing right now. Let's fix it!</p>
-            <p> Visit our <a href='https://www.opencollective.com/leaderboard'>Leaderboard</a> to find more collectives to support. </p>
-            </div>
-          }
+        <div className='Subscriptions-container'>
+          <SubscriptionItem />
         </div>
         <PublicFooter />
-        </div>
       </div>
     );
   }
