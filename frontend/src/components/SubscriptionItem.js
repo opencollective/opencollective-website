2b213d7fb45a3f95d471782ddf096c8e19f98f05
Ascari <carlos.ascari.x@gmail.com>
2016-07-26 19:08:11 -0500

    Added layout for donations list in SubscriptionItem

--- frontend/src/components/SubscriptionItem.js
+++ frontend/src/components/SubscriptionItem.js
@@ -4,9 +4,13 @@ export default class SubscriptionItem extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      opened: true
+    };
   }
 
   render() {
+    const { opened } = this.state;
     return (
       <div className='SubscriptionItem'>
         <div className='SubscriptionItem-header'>
@@ -24,7 +28,29 @@ export default class SubscriptionItem extends Component {
           </div>
         </div>
         <div className='SubscriptionItem-footer'>
-          <div className='footer-button'>show transactions</div>
+          {!opened && <div className='footer-button'>show transactions</div>}
+          <ul>
+            <li>
+              <div className='SubscriptionItem-DonationItem'>
+                <div className='flex'>
+                  <div>
+                    <img src='//d1ts43dypk8bqh.cloudfront.net/v1/avatars/1b16216d-38b1-45d6-b1d1-b89d0330fd68' height='40px' width='40px'/>
+                  </div>
+                  <div className='flex-auto'>
+                    <div className='flex flex-column'>
+                      <div className='flex'>
+                        <div className='flex-auto'>
+                          <div className='type'>Recurring Subscription</div>
+                        </div>
+                        <div className='amount'>$1.00<small>USD</small></div>
+                      </div>
+                      <div className='timeago'>1 month ago</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     )
