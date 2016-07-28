d5951568d5fd9f1aca86d18a0f62e18756c2a294
Ascari <carlos.ascari.x@gmail.com>
2016-07-28 13:09:31 -0500

    Subscriptions redesign (#376)
    * Added SubscriptionItem
    
    * Added layout for donations list in SubscriptionItem
    
    * Restructured Subscriptions container, began changing style
    
    * Displays real data: subscriptions and each transaction
    
    * Added responsive css
    cancelation when subscription is active

---
+++ frontend/src/components/SubscriptionItem.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+
+import formatCurrency from '../lib/format_currency';
+import moment from 'moment';
+
+export default class SubscriptionItem extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      opened: false
+    };
+  }
+
+  render() {
+    const { subscription, i18n, onClickImage } = this.props;
+    const { opened } = this.state;
+    const amount = subscription.amount;
+    const createdAt = subscription.createdAt
+    const currency = subscription.currency;
+    const interval = subscription.interval;
+    const isActive = subscription.isActive;
+    const formattedAmount = formatCurrency(amount, currency, {compact: true});
+    const formattedInterval = `${interval[0].toUpperCase()}${interval.substr(1)}ly`;
+    const formattedCreatedAt = isActive ? `${i18n.getString('since')} ${moment(createdAt).format('MMM, YYYY')}`: 'Inactive';
+    const Transactions = subscription.Transactions;
+    const Group = Transactions.length ? Transactions[0].Group : null;
+    const name = Group ? Group.name : '';
+    const image = Group ? Group.image : '';
+    return (
+      <div className='SubscriptionItem'>
+        <div className='SubscriptionItem-header'>
+          <div className='left'>{ name }</div>
+          <div className={`right ${isActive ? '' : 'canceled'}`} onClick={this.toggleActivation.bind(this)}>{`${isActive ? 'Cancel Subscription' : 'Canceled'}`}</div>
+        </div>
+        <div className='SubscriptionItem-body'>
+          <div className='SubscriptionItem-image' onClick={onClickImage} style={{display: image ? 'inline-block': 'none', backgroundImage: `url(${image})`}}></div>
+          <div className='SubscriptionItem-info'>
+            <div className='SubscriptionItem-info-amount'>
+              <span>{formattedAmount}</span>
+              <span className='currency-code'>{currency}</span>
+            </div>
+            <div className='SubscriptionItem-info-recurrence'>{`${formattedInterval} (${formattedCreatedAt})`}</div>
+          </div>
+        </div>
+        <div className='SubscriptionItem-footer'>
+          {opened && (
+            <ul>
+              {Transactions.map((transaction, index) => {
+                const description = transaction.title || transaction.description;
+                const avatar = transaction.User.avatar;
+                const txDate = transaction.incurredAt || transaction.createdAt;
+                return (
+                  <li key={index}>
+                    <div className='SubscriptionItem-DonationItem'>
+                      <div className='flex'>
+                        <div>
+                          <img src={avatar} height='40px' width='40px'/>
+                        </div>
+                        <div className='flex-auto'>
+                          <div className='flex flex-column'>
+                            <div className='flex'>
+                              <div className='flex-auto'>
+                                <div className='description'>{description}</div>
+                              </div>
+                              <div className='amount'>{formattedAmount}<small>{currency}</small></div>
+                            </div>
+                            <div className='time-ago'>{moment(txDate).fromNow()}</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                )
+              })}
+            </ul>
+          )}
+          <div className='footer-button' onClick={this.toggleOpened.bind(this)}>{`${opened ? 'hide' : 'show'} transactions`}</div>
+        </div>
+      </div>
+    )
+  }
+
+  toggleActivation() {
+    const { subscription, onCancel, onSubscribe } = this.props;
+    if (subscription.isActive) {
+      if (onCancel) onCancel(subscription.id);
+    } else {
+      if (onSubscribe) onSubscribe(subscription.id);
+    }
+  }
+
+  toggleOpened() {
+    this.setState({opened: !this.state.opened});
+  }
+}
--- frontend/src/containers/Subscriptions.js
+++ frontend/src/containers/Subscriptions.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import moment from 'moment';
 import { connect } from 'react-redux';
 import { pushState } from 'redux-router';
 
 import LoginTopBar from '../containers/LoginTopBar';
 import PublicFooter from '../components/PublicFooter';
-import Currency from '../components/Currency';
-import TransactionItem from '../components/TransactionItem';
 import Notification from '../containers/Notification';
-import InlineToggle from '../components/InlineToggle';
-import ProfilePhoto from '../components/ProfilePhoto';
+import SubscriptionItem from '../components/SubscriptionItem';
 
 import cancelSubscription from '../actions/subscriptions/cancel';
 import notify from '../actions/notification/notify';
@@ -21,86 +17,36 @@ export class Subscriptions extends Component {
   render() {
     const {
       subscriptions,
-      i18n,
       pushState
     } = this.props;
 
     return (
-      <div className='Subscription'>
-        <div className='container center'>
+      <div className='Subscriptions'>
         <LoginTopBar />
-
         <Notification />
-        <div className='PublicContent'>
-          <div className='Subscription-header'>
-            <div className='Subscription-page-title'>
-              <h3> Your Subscriptions </h3>
+        <div className='Subscriptions-container'>
+          {!subscriptions.length ? (
+            <div className='block-container center'>
+              <p> Looks like you aren't contributing right now. Let's fix it!</p>
+              <p> Visit our <a href='https://www.opencollective.com/leaderboard'>Leaderboard</a> to find more collectives to support. </p>
             </div>
-            {subscriptions.length > 0 &&
-              <div className='Subscription-user'>
-                 <ProfilePhoto
-                    hasBorder={true}
-                    url={subscriptions[0].Transactions[0].User && subscriptions[0].Transactions[0].User.avatar} />
-                {subscriptions[0].Transactions[0].User.name ?
-                  <span className='Subscription-user-name' title={subscriptions[0].Transactions[0].User.email}> {subscriptions[0].Transactions[0].User.name}</span> :
-                  <span className='Subscription-user-name'> {subscriptions[0].Transactions[0].User.email}</span>
-                }
-              </div>
-            }
-          </div>
-          {subscriptions.map(subscription => {
-              const divStyle = {
-                backgroundImage: `url(${subscription.Transactions[0].Group.logo})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'contain',
-                backgroundPosition: 'center'
-              };
-            return (
-                <div className='Subscription-group' key={subscription.id} >
-                  <div className='Subscription-logo' style={divStyle} onClick={() => pushState(null, `/${subscription.Transactions[0].Group.slug}`)} >
-                  </div>
-                  <div className='Subscription-info'>
-                    <div className='Subscription-name'>
-                        {subscription.Transactions[0].Group.name}
-                    </div>
-                    <div className='Subscription-amount'>
-                        <Currency value={subscription.amount} currency={subscription.currency} colorify={false}/> {subscription.interval}ly
-                        {subscription.isActive &&
-                        subscription.createdAt ?
-                          <span className='Subscription-status'>
-                            &nbsp;({i18n.getString('since')} {moment(subscription.createdAt).format('MMM YYYY')})
-                            {subscription.isActive &&
-                              <span className='Subscription-cancel' onClick={cancel.bind(this, subscription.id)}>CANCEL</span>}
-                          </span> :
-                        <span className='Subscription-status'> (Inactive) </span>
-                      }
-                    </div>
-                  </div>
-                    <div className='Subscription-transactions'>
-                      <InlineToggle showString='Show transactions' hideString='Hide transactions'>
-                        {subscription.Transactions.map(transaction => {
-                          return <TransactionItem key={transaction.id} transaction={transaction} i18n={i18n} />;
-                        })}
-                      </InlineToggle>
-                    </div>
-
-                </div>
-              );
-          })}
-          {subscriptions.length > 0 &&
-            <div className='Subscription-text Subscription-bottom-text'>
+          ) : null}
+          {subscriptions.length ? subscriptions.map(subscription => {
+            return (<SubscriptionItem 
+              key={subscription.id} 
+              subscription={subscription} 
+              onCancel={cancel.bind(this)}
+              onClickImage={() => pushState(null, `/${subscription.Transactions[0].Group.slug}`)}
+              {...this.props} />
+            )
+          }) : null}
+          {subscriptions.length ? (
+            <div className='block-container center'>
               <p> Visit our <a href='https://www.opencollective.com/leaderboard'>Leaderboard</a> to find more collectives to support! </p>
             </div>
-          }
-          {subscriptions.length === 0 &&
-            <div className='Subscription-text'>
-            <p> Looks like you aren't contributing right now. Let's fix it!</p>
-            <p> Visit our <a href='https://www.opencollective.com/leaderboard'>Leaderboard</a> to find more collectives to support. </p>
-            </div>
-          }
+          ) : null}
         </div>
         <PublicFooter />
-        </div>
       </div>
     );
   }
