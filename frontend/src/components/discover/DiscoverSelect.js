cf4b6b751ea1fa982811bf6fe4bf4c84ae796267
Ascari <carlos.ascari.x@gmail.com>
2016-08-01 11:19:25 -0500

    Added menu to DiscoverSelect component

--- frontend/src/components/discover/DiscoverSelect.js
+++ frontend/src/components/discover/DiscoverSelect.js
@@ -1,25 +1,37 @@
 import React, { Component, PropTypes } from 'react';
 
+const nbsp = '\xA0'; // non-breaking space: &nbsp;
+
+
 export default class DiscoverSelect extends Component {
 
   static propTypes = {}
 
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      opened: false
+    }
   }
 
   render() {
-    const { label, values } = this.props;
-    const current = 'all'
+    const { label, options, currentOption, onSelect } = this.props;
+    const { opened } = this.state;
+    const current = currentOption || '--'
     return (
       <div className='DiscoverSelect'>
         <div className='DiscoverSelect-label'>{ label }</div>
-        <div className='DiscoverSelect-selected'>
-          <span>{ current }</span>
+        <div className='DiscoverSelect-selected' onClick={() => this.setState({opened: !this.state.opened})}>
+          <span>{ currentOption }</span>
           <span className='arrow--bottom'></span>
         </div>
-
+        {opened ? (
+          <div className='DiscoverSelect-menu'>
+            <ul>
+              {options && options.map(option => <li onClick={() => {this.setState({opened: false}); onSelect(option)} } >{ option.replace(/\s+/g, nbsp) }</li>)}
+            </ul>
+          </div>
+        ) : null}
       </div>
     )
   }
--- frontend/src/containers/Discover.js
+++ frontend/src/containers/Discover.js
@@ -13,6 +13,10 @@ export class Discover extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      currentShowOption: null,
+      currentSortOption: null
+    };
   }
 
   componentDidMount() {
@@ -24,30 +28,40 @@ export class Discover extends Component {
   }
 
   render() {
-  	const { i18n } = this.props;
-  	return (
-  		<div className='Discover'>
-	        <LoginTopBar />
-	        <div className='Discover-container'>
-	        	<div className='Discover-hero'>
-	        		<div className='Discover-hero-cover'>
-	        			<div className='Discover-hero-line1'>Discover awesome collectives to support</div>
-	        			<div className='Discover-hero-line2'>Let's make great things together.</div>
-	        			<div className='Discover-hero-actions'>
-	        				<DiscoverSelect label='Show' />
-	        				<DiscoverSelect label='Sort by' />
-	        			</div>
-	        		</div>
-	        	</div>
-        		<div className='Discover-results'>
-        			<CollectiveCard i18n={i18n} />
-        			<CollectiveCard i18n={i18n} />
-        			<CollectiveCard i18n={i18n} />
-        		</div>
-	        </div>
-	        <PublicFooter />
-  		</div>
-  	)
+    const { i18n } = this.props;
+    const { currentShowOption, currentSortOption } = this.state;
+    const showOptions = ['Open Source', 'Meetup'];
+    const sortOptions = ['Newest'];
+    const results = [1,2,4,5,6,7];
+    
+    if (!currentShowOption && showOptions.length) {
+      this.setState({currentShowOption: showOptions[0]});
+    }
+    if (!currentSortOption && sortOptions.length) {
+      this.setState({currentSortOption: sortOptions[0]});
+    }
+
+    return (
+      <div className='Discover'>
+          <LoginTopBar />
+          <div className='Discover-container'>
+            <div className='Discover-hero'>
+              <div className='Discover-hero-cover'>
+                <div className='Discover-hero-line1'>Discover awesome collectives to support</div>
+                <div className='Discover-hero-line2'>Let's make great things together.</div>
+                <div className='Discover-hero-actions'>
+                  <DiscoverSelect label='Show' options={ showOptions } currentOption={ currentShowOption || '' } onSelect={option => this.setState({currentShowOption: option})} />
+                  <DiscoverSelect label='Sort by' options={ sortOptions } currentOption={ currentSortOption } onSelect={option => this.setState({currentSortOption: option})} />
+                </div>
+              </div>
+            </div>
+            <div className='Discover-results'>
+              {results.map(collective => <CollectiveCard i18n={i18n} />)}
+            </div>
+          </div>
+          <PublicFooter />
+      </div>
+    )
   }
 }
 
