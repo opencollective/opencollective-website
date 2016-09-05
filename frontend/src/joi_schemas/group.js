3acb6996d45013ce68a3bc008948e6e9b65be776
asood123 <asood123@users.noreply.github.com>
2016-09-05 14:55:51 -0400

    form to create a new group and add contributors (#418)
    * form to create a new group and add contributors
    
    * modifications from code review
    
    * filename issues
    
    * adding file back

---
+++ frontend/src/joi_schemas/group.js
@@ -0,0 +1,25 @@
+import Joi from 'joi';
+
+const schema = Joi.alternatives().try(
+  Joi.object().keys({
+    name: Joi.string().label('Name').required(),
+    slug: Joi.string().label('Slug').required(),
+    currency: Joi.string().label('Currency'),
+    website: Joi.string().uri().allow('').label('Website url'),
+    video: Joi.string().uri().allow('').label('Video url'),
+    image: Joi.string().uri().allow('').label('Image url'),
+    logo: Joi.string().uri().allow('').label('Logo url'),
+    mission: Joi.string().max(100).label('Mission'),
+    description: Joi.string().max(255).label('Short description'),
+    longDescription: Joi.string().max(1000).label('Full description'),
+    whyJoin: Joi.string().max(255).label('Why Join?'),
+    tags: Joi.string().label('Tags'),
+    users1: Joi.object().keys({name: Joi.string(), email: Joi.string().email(), twitterHandle: Joi.string()}),
+    users2: Joi.object().keys({name: Joi.string(), email: Joi.string().email(), twitterHandle: Joi.string()}),
+    users3: Joi.object().keys({name: Joi.string(), email: Joi.string().email(), twitterHandle: Joi.string()}),
+    users4: Joi.object().keys({name: Joi.string(), email: Joi.string().email(), twitterHandle: Joi.string()}),
+    users5: Joi.object().keys({name: Joi.string(), email: Joi.string().email(), twitterHandle: Joi.string()}),
+  })
+);
+
+export default schema;
