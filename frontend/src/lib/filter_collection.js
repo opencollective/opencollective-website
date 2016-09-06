14cdb44aec5858f54021a415f180158bd1763e71
Jonathan Soeder <jonathan.soeder@gmail.com>
2016-09-06 13:30:49 -0500

    Revert change to filter_collections util

--- frontend/src/lib/filter_collection.js
+++ frontend/src/lib/filter_collection.js
@@ -1,3 +1,4 @@
+import filter from 'lodash/filter';
 import values from 'lodash/values';
 
-export default (collection, validator) => values(collection).filter(validator);
+export default (collection, validator) => filter(values(collection), validator);
