f249eb6c0418d544ecb7cc91eb038ac6608bc6df
Ascari <carlos.ascari.x@gmail.com>
2016-09-23 11:39:40 -0500

    Added Navigation, top image and content for settings

---
+++ frontend/src/components/CustomInput.js
@@ -0,0 +1,5 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class CustomInput extends Component {
+  static propTypes = {}
+}
--- frontend/src/containers/Settings.js
+++ frontend/src/containers/Settings.js
@@ -5,6 +5,7 @@ import i18n from '../lib/i18n';
 
 import LoginTopBar from '../containers/LoginTopBar';
 import PublicFooter from '../components/PublicFooter';
+import Grid, { Column } from '../components/Grid';
 
 export class Settings extends Component {
   render() {
@@ -13,6 +14,28 @@ export class Settings extends Component {
       <div className='Settings'>
         <LoginTopBar />
         <div className='Settings-container'>
+          <div className='SettingsHead'></div>
+          <Grid flex>
+            <Column>
+              <div className='SettingsNav'>
+                <div className='SettingsNavItem'>General</div>
+                <div className='SettingsNavItem'>Social Integrations</div>
+                <div className='SettingsNavItem'>Mailing</div>
+                <div className='SettingsNavItem'>Banking</div>
+              </div>
+            </Column>
+            <Column auto>
+              <div className='SettingsPage'>
+                <div className='SettingsPageH1'>General</div>
+                <div className='SettingsPageHr'>
+                  <div>PayPal</div>
+                </div>
+                
+              </div>
+            </Column>
+          </Grid>
+
+
         Settings
         </div>
         <PublicFooter />
