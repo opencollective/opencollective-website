375d52e69e1788de6cf5877738ecb63bc6be84f2
Ascari <carlos.ascari.x@gmail.com>
2016-09-23 17:07:13 -0500

    Added CustomSelect component Structured SettingsGeneral component

--- frontend/src/components/settings/SettingsGeneral.js
+++ frontend/src/components/settings/SettingsGeneral.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
+import CustomSelect from '../CustomSelect';
+
 export default class SettingsGeneral extends Component {
   render() {
     return (
       <div className='SettingsGeneral'>
-        SettingsGeneral
+        <div className='SettingsPageH1'>General</div>
+        <div className='SettingsPageHr'>
+          <div>Language</div>
+        </div>
+        <CustomSelect>
+          <option>English</option>
+          <option>French</option>
+          <option>Spanish</option>
+        </CustomSelect>
+        <div className='SettingsPageHr'>
+          <div>Currency</div>
+        </div>
+        <CustomSelect>
+          <option>USD</option>
+          <option>EUR</option>
+          <option>MXN</option>
+        </CustomSelect>
+        <div className='SettingsPageButton' style={{marginTop: '40px'}}>Save</div>
       </div>
     )
   }
