f94bcbaff77972b60e15ab7add78cb3855b8c544
Xavier Damman <xdamman@gmail.com>
2016-09-26 17:44:58 -0400

    Support for multipage in longDescription

---
+++ frontend/src/lib/process_markdown.js
@@ -0,0 +1,37 @@
+/**
+ * This breaks down a markdown text into multiple pages
+ * @PRE: 
+ *   intro text
+ *   # title1
+ *   Hello
+ *   # title2
+ *   World
+ * @POST: { 
+ *  intro: "intro text", 
+ *  title1: "Hello", 
+ *  title2: "World" 
+ * }
+ */
+export default function processMarkdown(text) {
+
+  const lines = text.split('\n');
+
+  const body = {};
+  let currentTitle = 'intro';
+  let paragraph = [];
+  lines.forEach((line) => {
+    if (line.length <= 20) {
+      const match = line.match(/^# *([^#]{2,20})/i);
+      if (match) {
+        body[currentTitle] = paragraph.join('\n').trim();
+        currentTitle = match[1];
+        paragraph = [];
+        return;
+      }
+    }
+    paragraph.push(line);
+  });
+  body[currentTitle] = paragraph.join('\n').trim();
+
+  return body;
+}
