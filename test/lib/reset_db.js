43d5678fd047eabcb1286382b0519303e4f57eaf
Xavier Damman <xdamman@gmail.com>
2016-10-04 01:31:50 +0200

    making sure we send the api key to reset the db

--- test/lib/reset_db.js
+++ test/lib/reset_db.js
@@ -1,7 +1,7 @@
 const config = require('config');
 
 module.exports = client => {
-  const resetUrl = `${config.host.api}/database/reset`;
+  const resetUrl = `${config.host.api}/database/reset?api_key=${config.apiKey}`;
   return client
     .url(resetUrl)
     .assert.containsText('body', '\"success\":true', `checking ${resetUrl} call succeeded`);
