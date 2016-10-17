a818e9de10980f3b8c5574cbde1bf15431839560
Xavier Damman <xdamman@gmail.com>
2016-10-17 11:30:01 +0200

    max length for description to 255

--- frontend/src/joi_schemas/profile.js
+++ frontend/src/joi_schemas/profile.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 const schema = Joi.alternatives().try(
   Joi.object().keys({
     name: Joi.string().allow('').label('Name'),
-    description: Joi.string().max(15).allow('').label('description'),
+    description: Joi.string().max(255).allow('').label('description'),
     longDescription: Joi.string().allow('').label('long description'),
     website: Joi.string().uri().allow('').label('Website'),
     twitterHandle: Joi.string().allow('').label('Twitter username'),
