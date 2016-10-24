a38c6dfd5a334d5ea40733eade9afe3c975d4a32
Aseem Sood <asood123@yahoo.com>
2016-10-24 17:45:11 -0400

    fix bug in avatar row

--- frontend/src/components/UserAvatarRow.js
+++ frontend/src/components/UserAvatarRow.js
@@ -2,7 +2,7 @@ import React, {Component, PropTypes} from 'react';
 
 import unionBy from 'lodash/unionBy';
 
-const MAX_SHOWING = 15;
+const MAX_SHOWING = 12;
 
 export default class UserAvatarRow extends Component {
 
@@ -12,19 +12,19 @@ export default class UserAvatarRow extends Component {
       members
     } = this.props;
 
-    const users = unionBy(members, backers, 'username');
-    console.log(users);
-    const usersToShow = users.filter(u => u.avatar ? true : false).slice(MAX_SHOWING);
-    if (users.length === 0 || usersToShow.length === 0 ) {
+    const all = unionBy(members, backers, 'username');
+    const toShow = all.filter(u => u.avatar && u.avatar.indexOf('http') !== -1 ? true : false).slice(0, MAX_SHOWING-1);
+
+    if (all.length === 0 || toShow.length === 0 ) {
       return (<div />);
     }
     return (
       <div className='UserAvatarRow'>
         <span className='UserAvatarRow-shots'>
-          {usersToShow.map(u =>
+          {toShow.map(u =>
             <span className='UserAvatarRow-avatar' key={u.username} style={{backgroundImage: `url(${u.avatar})`}} />)}
         </span>
-        <span className='UserAvatarRowCount'> <a href='#contributors'> {`+${users.length - usersToShow.length}`} </a></span>
+        <span className='UserAvatarRowCount'> <a href='#contributors'> {`+${all.length - toShow.length}`} </a></span>
       </div>
     );
   }
