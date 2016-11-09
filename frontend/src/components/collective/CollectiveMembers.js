f0b6158c8dc919c52400eff7e5f3d84be5c19a0f
Xavier Damman <xdamman@gmail.com>
2016-11-09 15:30:33 -0500

    show the user cards if contributors.length < 10

--- frontend/src/components/collective/CollectiveMembers.js
+++ frontend/src/components/collective/CollectiveMembers.js
@@ -5,24 +5,19 @@ import _ from 'lodash';
 
 export default class CollectiveMembers extends React.Component {
 
-  constructor(props) {
-    super(props);
-    const { group } = props;
-    this.contributors = _.uniqBy(_.union(group.members, group.backers.filter(b => !b.tier.match(/sponsor/i))), 'id');
-    this.contributors = this.contributors.map(c => {
-      c.href = `/${c.username}`;
-      return c;
-    });
-    this.sponsors = _.uniqBy(group.backers.filter(b => b.tier.match(/sponsor/i)), 'id');
-  }
-
   _showCards(users) {
     const { i18n } = this.props;
     return users.map((user, j) => (<UserCard ref={`UserCard-${j}`} user={user} key={j} className='m1' i18n={i18n} />));
   }
 
   render() {
-    const { i18n } = this.props;
+    const { i18n, group } = this.props;
+    this.contributors = _.uniqBy(_.union(group.members, group.backers.filter(b => !b.tier.match(/sponsor/i))), 'id');
+    this.contributors = this.contributors.map(c => {
+      c.href = `/${c.username}`;
+      return c;
+    });
+    this.sponsors = _.uniqBy(group.backers.filter(b => b.tier.match(/sponsor/i)), 'id');
     return (
       <section id='contributors' className='Collective-MembersWall relative'>
         <div className='CollectiveBackers container center relative'>
@@ -30,7 +25,11 @@ export default class CollectiveMembers extends React.Component {
             <h2 className='Collective-title m0 -ff-sec -fw-bold'>{i18n.getString('membersWallTitle')}</h2>
             <p className='Collective-font-17 max-width-3 mx-auto mb3'>{i18n.getString('membersWallText')}</p>
             <div className='CollectiveWhoWeAre-contributors' className='flex flex-wrap justify-center'>
+            { this.contributors.length < 10 &&
+              this._showCards(this.contributors)}
+            { this.contributors.length > 10 &&
               <Mosaic users={this.contributors} i18n={i18n} />
+            }
             </div>
             <div className='CollectiveWhoWeAre-sponsors' className='flex flex-wrap justify-center'>
               {this._showCards(this.sponsors)}
--- frontend/src/components/public_group/PublicGroupMembersWall.js
+++ frontend/src/components/public_group/PublicGroupMembersWall.js
@@ -6,24 +6,19 @@ import _ from 'lodash';
 
 export default class PublicGroupMembersWall extends React.Component {
 
-  constructor(props) {
-    super(props);
-    const { group } = props;
-    this.contributors = _.uniqBy(_.union(group.members, group.backers.filter(b => !b.tier.match(/sponsor/i))), 'id');
-    this.contributors = this.contributors.map(c => {
-      c.href = `/${c.username}`;
-      return c;
-    });
-    this.sponsors = _.uniqBy(group.backers.filter(b => b.tier.match(/sponsor/i)), 'id');
-  }
-
   _showCards(users) {
     const { i18n } = this.props;
     return users.map((user, j) => (<UserCard ref={`UserCard-${j}`} user={user} key={j} className='m1' i18n={i18n} />));
   }
 
   render() {
-    const { i18n } = this.props;
+    const { i18n, group } = this.props;
+    this.contributors = _.uniqBy(_.union(group.members, group.backers.filter(b => !b.tier.match(/sponsor/i))), 'id');
+    this.contributors = this.contributors.map(c => {
+      c.href = `/${c.username}`;
+      return c;
+    });
+    this.sponsors = _.uniqBy(group.backers.filter(b => b.tier.match(/sponsor/i)), 'id');
     return (
       <section id='contributors' className='PublicGroup-MembersWall relative'>
         <div className='PublicGroupBackers container center relative'>
@@ -31,7 +26,11 @@ export default class PublicGroupMembersWall extends React.Component {
             <h2 className='PublicGroup-title m0 -ff-sec -fw-bold'>{i18n.getString('membersWallTitle')}</h2>
             <p className='PublicGroup-font-17 max-width-3 mx-auto mb3'>{i18n.getString('membersWallText')}</p>
             <div className='PublicGroupWhoWeAre-contributors' className='flex flex-wrap justify-center'>
+            { this.contributors.length < 10 &&
+              this._showCards(this.contributors)}
+            { this.contributors.length > 10 &&
               <Mosaic users={this.contributors} i18n={i18n} />
+            }
             </div>
             <div className='PublicGroupWhoWeAre-sponsors' className='flex flex-wrap justify-center'>
               {this._showCards(this.sponsors)}
