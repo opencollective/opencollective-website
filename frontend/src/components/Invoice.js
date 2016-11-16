d2e65cc7088891b8d28371c82e2ae2fcb261bb57
Xavier Damman <xdamman@gmail.com>
2016-11-17 00:15:14 +0100

    added PDF invoice

---
+++ frontend/src/components/Invoice.js
@@ -0,0 +1,65 @@
+import React, { Component, PropTypes } from 'react';
+import Currency from './Currency';
+import { resizeImage } from '../lib/utils';
+import Table from 'rc-table';
+
+export default class Invoice extends Component {
+
+  constructor(props) {
+    super(props);
+    this.styles = {
+      footer: {
+        paddingLeft: '480'
+      }
+    };
+  }
+
+  render() {
+    const {
+      transactions,
+      user,
+      i18n
+    } = this.props;
+
+    const createdAt = new Date(transactions[0].createdAt);
+    const year = createdAt.getFullYear();
+    const month = createdAt.getMonth() + 1;
+
+    const columns = [
+      {title: 'date', dataIndex: 'date', className: 'date' },
+      {title: 'description', dataIndex: 'description', key: 'description', width: 400},
+      {title: 'amount', dataIndex: 'amount', key: 'amount'},
+    ];
+
+    let total = 0;
+    let currency = 'USD';
+    const data = transactions.map(t => {
+      total += t.amount;
+      currency = t.currency;
+      return {
+        date: i18n.moment(t.createdAt).format('MM/DD'),
+        description: t.description,
+        amount: <Currency value={t.amount * 100} currency={t.currency} />
+      }
+    });
+
+    data.push({
+      description: "Total",
+      amount: <Currency value={total * 100} currency={currency} />
+    });
+
+    return (
+      <div className='Invoice'>
+          <h1>{i18n.moment(`${year}-${month}`).format('MMMM')} {year} Invoice</h1>
+          <div className="reference">Reference: {user.username}-{year}-{month}-1</div>
+          <Table columns={columns} data={data} rowClassName={(row, index) => (index === data.length - 1) ? `footer` : ''} />
+      </div>
+    );
+  }
+}
+
+Invoice.PropTypes = {
+  i18n: PropTypes.object.isRequired,
+  transactions: PropTypes.object,
+  user: PropTypes.object.isRequired
+};
