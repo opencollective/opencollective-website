60ccc5658e21d5678fe1621e4017f44898272547
Xavier Damman <xdamman@gmail.com>
2016-12-05 18:41:50 -0500

    small css fix

--- frontend/src/components/collective/CollectiveDonate.js
+++ frontend/src/components/collective/CollectiveDonate.js
@@ -29,7 +29,7 @@ export default class CollectiveDonate extends React.Component {
     return (
       <section id='join-us'>
         <div id='support'></div>
-        <div className='CollectiveDonate-container container center'>
+        <div className='CollectiveDonate-container center'>
           <h2 className='Collective-title m0 pb2 -ff-sec -fw-bold'>{i18n.getString('joinAndFulfil')}</h2>
           <p className='Collective-font-17 m0 pb2'>{i18n.getString('helpUsContinueOurActivities')}</p>
           <div className='flex flex-wrap justify-center clearfix max-width-4 mx-auto pt3'>
