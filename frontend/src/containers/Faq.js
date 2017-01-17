11f1516fe7a99e583945356a27fb3a95780ae63c
Xavier Damman <xdamman@gmail.com>
2017-01-17 10:56:01 -0500

    updated faq (#622)

--- frontend/src/containers/Faq.js
+++ frontend/src/containers/Faq.js
@@ -42,12 +42,12 @@ export class Faq extends Component {
           <p>Open Collective is perfect for any side project that is not for profit. Side projects should be fun. You shouldn't have to worry about creating and maintaining a legal entity, doing bookkeeping, etc. as soon as you need to collect money to reimburse your expenses. Open Collective is ideal for meetups, open source projects, parent associations, neighborhood associations, clubs, unions, movements, non-profits, business incubators, etc.</p>
 
           <h3>Why should I use Open Collective?</h3>
-          <p>Open Collective makes it easy to be transparent about your finances. We help you collect money <em>recurringly</em> for your group. Think about funding an ongoing open source software, collecting dues for a parent association or charge a monthly fee for your food coop. Once you have the money on Open Collective, it’s easy to spend it and everyone can see how it’s being spent. We believe that financial transparency in a group will create stronger and more efficient collaboration among people.</p>
+          <p>Open Collective makes it easy to be transparent about your finances. We help you collect money <em>recurringly</em> for your community. Think about funding an ongoing open source software, collecting dues for a parent association or charge a monthly fee for your food coop. Once you have the money on Open Collective, it’s easy to spend it and everyone can see how it’s being spent. We believe that financial transparency in a community will create stronger and more efficient collaboration among people.</p>
 
           <h3>How is that different from other crowdfunding platforms?</h3>
           <p>Two major differences:</p>
           <ul>
-            <li>Open Collective helps you raise money recurringly (e.g. monthly) to finance the activities of your group. That way, you can plan your future activities against your anticipated monthly budget.</li>
+            <li>Open Collective helps you raise money recurringly (e.g. monthly) to finance the activities of your community. That way, you can plan your future activities against your anticipated monthly budget.</li>
             <li>Open Collective allows (and encourages) full transparency and accountability of the money raised. Other platforms hand over the money without showing to the backers how their money is used. On Open Collective, in order to use the money, you submit an expense for everyone to see. We believe transparency leads to healthier organizations.</li>
           </ul>
 
@@ -112,60 +112,67 @@ export class Faq extends Component {
           <p>Shoot us an email at support@opencollective.com or drop by our Slack channel https://slack.opencollective.com</p>
           
           <h3 id="investors">Who are your investors?</h3>
-          <p>We did a first pre-seed round of $500k in October 2015 (<a href="https://www.ycombinator.com/docs/SAFE_Cap.rtf">SAFE</a>, $5M cap) with:
-            <ul>
-              <li><span>$250k</span> <a href="http://generalcatalyst.com">General Catalyst</a> (SF/NYC/Boston, <a href="https://www.linkedin.com/in/hemanttaneja">Hemant Taneja</a>)</li>
-              <li><span>$50k</span> <a href="https://www.linkedin.com/in/jsiegel">Jonathan Siegel</a> (can't pin down his location)</li>
-              <li><span>$50k</span> <a href="http://belcube.com">Belcube</a> (Brussels)</li>
-              <li><span>$50k</span> <a href="https://www.linkedin.com/in/teljamou">Tony Jamous</a> (London, <a href="https://nexmo.com">Nexmo</a>)</li>
-              <li><span>$25k</span> <a href="https://uk.linkedin.com/in/enadalin">Eric Nadalin</a> (London, <a href="https://nexmo.com">Nexmo</a>)</li>
-              <li><span>$25k</span> <a href="https://www.linkedin.com/in/brian-larson-43904010">Brian Larson</a> (SF, engineer at Google, Twitter)</li>
-              <li><span>$15k</span> <a href="https://twitter.com/rauchg">Guillermo Rauch</a> (SF/Buenos Aires, <a href="http://socket.io">socket.io</a>)</li>
-              <li><span>$10k</span> <a href="http://buytaert.net">Dries Buytaert</a> (Boston, <a href="https://drupal.org">Drupal</a>)</li>
-              <li><span>$10k</span> <a href="http://assemble.vc">Assemble.vc</a> (Boston)</li>
-              <li><span>$5k</span> <a href="https://www.linkedin.com/in/toonvanagt">Toon Vanagt</a> (Brussels, <a href="http://data.be">data.be</a>)</li>
-              <li><span>$5k</span> <a href="https://www.linkedin.com/in/xaviercorman">Xavier Corman</a> (Brussels, <a href="http://edebex.com">Edebex</a>)</li>
-              <li><span>$5k</span> Personal friend</li>
-            </ul>
-          </p>
+          <details>
+            <summary>We raised a total of $805k so far on convertible notes (<a href="https://www.ycombinator.com/docs/SAFE_Cap.rtf">SAFE</a>)</summary>
 
-          <p>We did a follow up round of $305k in July 2016 (<a href="https://www.ycombinator.com/docs/SAFE_Cap.rtf">SAFE</a>, $8M cap) with:
-            <ul>
-              <li><span>$100k</span> <a href="https://www.linkedin.com/in/ricardo-gorodisch-9b057889">Ricardo Gorodisch</a> (Argentina, President <a href="http://www.fundacionkaleidos.org/">Foundation Kaleidos</a>)</li>
-              <li><span>$50k</span> <a href="https://www.linkedin.com/in/petekoomen">Pete Koomen</a> (SF, Cofounder/CTO <a href="http://optimizely.com">Optimizely</a>)</li>
-              <li><span>$25k</span> <a href="https://www.linkedin.com/in/jpayne">Jim Payne</a> (NYC, Cofounder MoPub, EIR Accel Partners)</li>
-              <li><span>$25k</span> <a href="https://www.linkedin.com/in/caesar-sengupta-2743b">Caesar Sengupta</a> (Bay Area, VP Product Management at Google)</li>
-              <li><span>$20k</span> <a href="https://www.linkedin.com/in/gkgandhi">Gautam Gandhi</a> (India, Entrepreneur, former Head New Business Development India at Google)</li>
-              <li><span>$15k</span> <a href="https://www.linkedin.com/in/tpbrown5">Tom Brown</a> (FinTech lawyer, partner at Paul Hastings, former VP at Visa )</li>
-              <li><span>$15k</span> Vadim (NYC/Bay Area/Buenos Aires)</li>
-              <li><span>$10k</span> <a href="https://www.linkedin.com/in/johnkobs">John Kobs</a> (SF, Entrepreneur/CEO at <a href="http://ApartmentList.com">ApartmentList</a>)</li>
-              <li><span>$10k</span> <a href="https://www.linkedin.com/in/nicolaswittenborn">Nicolas Wittenborn</a> (Berlin, principal at point 9 venture)</li>
-              <li><span>$10k</span> <a href="https://www.linkedin.com/in/derek-parham-b7b5504">Derek Parham</a> (NYC, deputy CTO at Hillary For America)</li>
-              <li><span>$10k</span> <a href="https://www.linkedin.com/in/hbridge">Henry Bridge</a> (NYC, Director of Product at Hillary For America)</li>
-              <li><span>$10k</span> <a href="https://www.linkedin.com/in/antoineperdaens">Antoine Perdaens</a> (Belgium, Cofounder/CEO at <a href="http://knowledgeplaza.net">KnowledgePlaza</a>)</li>
-              <li><span>$10k</span> <a href="https://www.linkedin.com/in/sam-de-brouwer-b0a34122">Sam De Brouwer</a> (SF, <a href="http://www.tedxsanfrancisco.com">TEDxSanFrancisco</a>)</li>
-            </ul>
-          </p>
+            <p>We did a first pre-seed round of $500k in October 2015 (SAFE, $5M cap) with:
+              <ul>
+                <li><span>$250k</span> <a href="http://generalcatalyst.com">General Catalyst</a> (SF/NYC/Boston, <a href="https://www.linkedin.com/in/hemanttaneja">Hemant Taneja</a>)</li>
+                <li><span>$50k</span> <a href="https://www.linkedin.com/in/jsiegel">Jonathan Siegel</a> (can't pin down his location)</li>
+                <li><span>$50k</span> <a href="http://belcube.com">Belcube</a> (Brussels)</li>
+                <li><span>$50k</span> <a href="https://www.linkedin.com/in/teljamou">Tony Jamous</a> (London, <a href="https://nexmo.com">Nexmo</a>)</li>
+                <li><span>$25k</span> <a href="https://uk.linkedin.com/in/enadalin">Eric Nadalin</a> (London, <a href="https://nexmo.com">Nexmo</a>)</li>
+                <li><span>$25k</span> <a href="https://www.linkedin.com/in/brian-larson-43904010">Brian Larson</a> (SF, engineer at Google, Twitter)</li>
+                <li><span>$15k</span> <a href="https://twitter.com/rauchg">Guillermo Rauch</a> (SF/Buenos Aires, <a href="http://socket.io">socket.io</a>)</li>
+                <li><span>$10k</span> <a href="http://buytaert.net">Dries Buytaert</a> (Boston, <a href="https://drupal.org">Drupal</a>)</li>
+                <li><span>$10k</span> <a href="http://underscore.vc">Underscore.vc</a> (Boston)</li>
+                <li><span>$5k</span> <a href="https://www.linkedin.com/in/toonvanagt">Toon Vanagt</a> (Brussels, <a href="http://data.be">data.be</a>)</li>
+                <li><span>$5k</span> <a href="https://www.linkedin.com/in/xaviercorman">Xavier Corman</a> (Brussels, <a href="http://edebex.com">Edebex</a>)</li>
+                <li><span>$5k</span> Personal friend</li>
+              </ul>
+            </p>
+
+            <p>We did a follow up round of $305k in July 2016 (SAFE, $8M cap) with:
+              <ul>
+                <li><span>$100k</span> <a href="https://www.linkedin.com/in/ricardo-gorodisch-9b057889">Ricardo Gorodisch</a> (Argentina, President <a href="http://www.fundacionkaleidos.org/">Foundation Kaleidos</a>)</li>
+                <li><span>$50k</span> <a href="https://www.linkedin.com/in/petekoomen">Pete Koomen</a> (SF, Cofounder/CTO <a href="http://optimizely.com">Optimizely</a>)</li>
+                <li><span>$25k</span> <a href="https://www.linkedin.com/in/jpayne">Jim Payne</a> (NYC, Cofounder MoPub, EIR Accel Partners)</li>
+                <li><span>$25k</span> <a href="https://www.linkedin.com/in/caesar-sengupta-2743b">Caesar Sengupta</a> (Bay Area, VP Product Management at Google)</li>
+                <li><span>$20k</span> <a href="https://www.linkedin.com/in/gkgandhi">Gautam Gandhi</a> (India, Entrepreneur, former Head New Business Development India at Google)</li>
+                <li><span>$15k</span> <a href="https://www.linkedin.com/in/tpbrown5">Tom Brown</a> (FinTech lawyer, partner at Paul Hastings, former VP at Visa )</li>
+                <li><span>$15k</span> Vadim (NYC/Bay Area/Buenos Aires)</li>
+                <li><span>$10k</span> <a href="https://www.linkedin.com/in/johnkobs">John Kobs</a> (SF, Entrepreneur/CEO at <a href="http://ApartmentList.com">ApartmentList</a>)</li>
+                <li><span>$10k</span> <a href="https://www.linkedin.com/in/nicolaswittenborn">Nicolas Wittenborn</a> (Berlin, principal at point 9 venture)</li>
+                <li><span>$10k</span> <a href="https://www.linkedin.com/in/derek-parham-b7b5504">Derek Parham</a> (NYC, deputy CTO at Hillary For America)</li>
+                <li><span>$10k</span> <a href="https://www.linkedin.com/in/hbridge">Henry Bridge</a> (NYC, Director of Product at Hillary For America)</li>
+                <li><span>$10k</span> <a href="https://www.linkedin.com/in/antoineperdaens">Antoine Perdaens</a> (Belgium, Cofounder/CEO at <a href="http://knowledgeplaza.net">KnowledgePlaza</a>)</li>
+                <li><span>$10k</span> <a href="https://www.linkedin.com/in/sam-de-brouwer-b0a34122">Sam De Brouwer</a> (SF, <a href="http://www.tedxsanfrancisco.com">TEDxSanFrancisco</a>)</li>
+              </ul>
+            </p>
+          </details>
 
           <h3 id="wdydwyd">Why Do You Do What You Do? <a href="https://twitter.com/search?f=images&vertical=default&q=%23wdydwyd&src=typd" target="_blank">#wdydwyd</a></h3>
-          <p>We want to empower people to create the organizations and institutions of tomorrow.</p>
+          <p>We want to empower people to create the organizations and institutions of tomorrow, one community at a time.</p>
 
-          <p>After <a href="https://Storify.com">Storify</a> (my first startup in Silicon Valley), one of my options was to go back to Belgium (where I’m originally from) and start a new political party or a “union 2.0”. Why? Because 90%+ of the young people there don’t feel represented by existing political parties or unions (and I’m pretty sure it’s the same in your country). All those institutions were founded 100+ years ago at a time when paper was the only two-way communication channel. They lack feedback loops, they lack transparency and as a result, they are increasingly disconnected from their base. We need new institutions for this Internet era.</p>
+          <details>
+            <summary>
+              "I want to empower people to go build the organizations, movements, associations and institutions of tomorrow in a bottom-up fashion where transparency is built in by design."
+              – <a href="https://twitter.com/xdamman">Xavier</a>, cofounder
+            </summary>
+              <p>After <a href="https://Storify.com">Storify</a> (my first startup in Silicon Valley), one of my options was to go back to Belgium (where I’m originally from) and start a new political party or a “union 2.0”. Why? Because 90%+ of the young people there don’t feel represented by those institutions (and I’m pretty sure it’s the same in your country). They were founded 100+ years ago (BI -- Before Internet) at a time when paper was the only communication channel readily available. They lack feedback loops, they lack transparency and as a result, they are increasingly disconnected from reality. We need new institutions for this Internet era.</p>
 
-          <p>I could spend the next 5-10 years of my life doing that. But that doesn't scale. Yet, this is a global issue. It's not limitd to Belgium. So I wondered: “How can I empower others to go build those political parties and unions of tomorrow?”; “What tools do people need to create those organizations?”.</p>
+              <p>I could spend the next 5-10 years of my life doing that. But that doesn't scale. Yet, this is a global issue. It's not limited to Belgium. So I wondered: “How can I empower others to go build those institutions of tomorrow?”; “What tools do people need to create those organizations?”.</p>
 
-          <p>While the Internet has been really good so far at helping people get together and do things together, it has been quite bad at dealing with money. As a result, we’ve seen a lot of movements popping up here and there that ended up disappearing as quickly. The harsh reality of this world is that without money, you can’t sustain a group over time.</p>
+              <p>While the Internet has been really good so far at helping people get together and do things together, it has been quite bad at dealing with money. As a result, we’ve seen a lot of movements popping up here and there that ended up disappearing as quickly. The harsh reality of this world is that without money, you can’t sustain a community over time.</p>
 
-          <p>The biggest notable exception is <a href="https://en.wikipedia.org/wiki/Podemos_%28Spanish_political_party%29#cite_note-GPnov2014-54">Podemos</a>, the new political party in Spain that started in 2014 and who quickly became the second largest political party just a year later. Their secret? They were able to get 285,000+ people to become members and pay a minimum of 5 euros (~$6) per month to finance and sustain the movement.</p>
+              <p>The biggest notable exception is <a href="https://en.wikipedia.org/wiki/Podemos_%28Spanish_political_party%29#cite_note-GPnov2014-54">Podemos</a>, the new political party in Spain that started in 2014 and who quickly became the second largest political party just a year later. Their secret? They were able to get 285,000+ people to become members and pay a minimum of 5 euros (~$6) per month to finance and sustain the movement.</p>
 
-          <p>Unfortunately, it’s still too complicated for a group to quickly collect money for their cause.</p>
+              <p>Unfortunately, it’s still too complicated for a community to quickly collect money for their cause.</p>
 
-          <p>I experienced it first hand when I lead the <a href="http://startupmanifesto.be">Startup Manifesto in Belgium</a>. A grassroots movement to come up with recommendations to the government to “make it suck less” for startups in Belgium. At some point, we wanted to print stickers for a conference but we didn’t have money. Plenty of people were willing to support our cause and make a donation. But in order to accept that money, we would have to create a bank account, which requires first to create a legal entity. We had better things to do. Not to mention that the money would have been collected in a black box where all supporters would have zero visibility. That was against our values. So we just gave up on the idea of collecting money. And <a href="https://twitter.com/xdamman/status/575744358592475136">an existing organization simply paid the stickers for us</a>.</p>
+              <p>I experienced it first hand when I lead the <a href="http://startupmanifesto.be">Startup Manifesto in Belgium</a>. A grassroots movement to come up with recommendations to the government to “make it suck less” for startups in Belgium. At some point, we wanted to print stickers for a conference but we didn’t have money. Plenty of people were willing to support our cause and make a donation. But in order to accept that money, we would have to create a bank account, which requires first to create a legal entity. We had better things to do. Not to mention that the money would have been collected in a black box where all supporters would have zero visibility. That was against our values. So we just gave up on the idea of collecting money. And <a href="https://twitter.com/xdamman/status/575744358592475136">an existing organization simply paid the stickers for us</a>.</p>
 
-          <p>That’s why I’m building Open Collective.<br />
-            I want to empower people to do things together with money. I want to empower people to go build the organizations, movements, associations and institutions of tomorrow in a bottom-up fashion where transparency is built in by design.</p>
-
-            <p>– <a href="https://twitter.com/xdamman">Xavier</a>, founder</p>
+              <p>That’s why I’m building Open Collective.</p>
+            </details>
         </div>
         <PublicFooter />
       </div>
