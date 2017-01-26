f6568e2b65280b39391844757bb89139bc46039c
Xavier Damman <xdamman@gmail.com>
2017-01-26 15:29:21 +0100

    match the size of the mosaic with its container

--- frontend/src/components/collective/GithubContributors.js
+++ frontend/src/components/collective/GithubContributors.js
@@ -1,6 +1,31 @@
 import React, { PropTypes } from 'react';
+import debounce from 'lodash/debounce';
 
 export default class GithubContributors extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { width: 0 };
+  }
+
+  componentDidMount() {
+    this.el = React.findDOMNode(this)
+    this.resizeMosaic();
+    window.addEventListener("resize", this.debouncedResizeMosaic.bind(this));
+  }
+
+  resizeMosaic() {
+    const styles = window.getComputedStyle(this.el.parentNode, null);
+    let width = this.el.parentNode.getBoundingClientRect().width - parseInt(styles.paddingLeft, 10) - parseInt(styles.paddingRight, 10);
+    width = Math.min(Math.floor(width / 60) * 60, 1000); // round down to the closest multiple of 60 to avoid requesting too many different filesizes
+    this.setState({width});
+  }
+
+  debouncedResizeMosaic() {
+    debounce(() => {
+      this.resizeMosaic();
+    }, 500)();
+  }
 
  render() {
   const { collective, i18n } = this.props;
@@ -22,7 +47,7 @@ export default class GithubContributors extends React.Component {
   return (
     <div className='GithubContributors'>
       <div className='line1' >+{ collective.contributors.length } {i18n.getString('contributorsOnGithub')}</div>
-      <a href={githubUrl} target="_blank"><img width="640" src={`/${collective.slug}/contributors.png?avatarHeight=${avatarHeight}&button=false&width=640&margin=${margin}`} /></a>
+      <a href={githubUrl} target="_blank"><img width={this.state.width} src={`/${collective.slug}/contributors.png?avatarHeight=${avatarHeight}&button=false&width=${this.state.width}&margin=${margin}`} /></a>
     </div>
     );
   }
