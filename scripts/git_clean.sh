a3cb4228534fc87d89d024c6df7f7e3a9c04f2dd
Xavier Damman <xdamman@gmail.com>
2017-03-07 14:18:12 -0500

    better wording

--- scripts/git_clean.sh
+++ scripts/git_clean.sh
@@ -2,13 +2,14 @@
 if [[ `git checkout master` ]]; then 
   for BRANCH in `git branch | grep -v "*"`; do
     if [[ `git diff master..$BRANCH` ]]; then
-      echo "- $BRANCH needs to be fast-forward";
+      echo "> $BRANCH needs to be merged";
       git checkout $BRANCH;
       if git merge master -m "merging master into $BRANCH" 1> /dev/null; then
         git checkout master;
 	git merge $BRANCH --ff-only;
         git branch -d $BRANCH;
       else
+	echo "$BRANCH cannot be merged automatically, aborting";
 	git merge --abort;
       fi;
     else 
