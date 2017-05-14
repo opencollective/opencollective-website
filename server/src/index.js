e92596ce986d11ca3d82be85cb3457afdb2b551f
Xavier Damman <xdamman@gmail.com>
2017-05-13 20:57:49 -0400

    using sepia to record API requests

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,17 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   require('dotenv').load();
 }
 
+import sepia from 'sepia';
+import path from 'path';
+
+sepia.fixtureDir(path.join(process.cwd(), '/test/fixtures'));
+if (process.env.DEBUG && process.env.DEBUG.match(/sepia/)) {
+  sepia.configure({
+    verbose: true,
+    debug: true
+  });
+}
+
 import 'babel-register';
 import './global';
 
--- test/e2e/connect_twitter_button.js
+++ test/e2e/connect_twitter_button.js
@@ -11,11 +11,6 @@ module.exports = {
     .assert.containsText('body', 'CONNECT TO TWITTER'),
 
   'Redirects to Twitter after clicking connect': (client) => {
-
-    // const clientId = config.github.clientId;
-    // const callbackUrl = encodeURIComponent(`${config.host.website}/auth/twitter/callback?slug=testcollective`);
-    // const githubScope = encodeURIComponent('user:email');
-
     client
       .click('.connectAccountBtn')
       .waitForElementVisible('body', 2000)
@@ -23,12 +18,6 @@ module.exports = {
       .setValue('input[id=username_or_email]', config.twitter.testUsername)
       .setValue('input[id=password]', config.twitter.testPassword)
       .click('input[id=allow]') // click 'Sign In'
-
-      // This part is only necessary when user wasn't granted a token yet, so must be commented out after first run
-      // .waitForElementVisible('body', 2000)
-      // .assert.urlContains(`https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${callbackUrl}&response_type=code&scope=${githubScope}`)
-      // .click('button[name=authorize]') // click 'Authorize application'
-
       .waitForElementVisible('body', 2000)
       .assert.urlContains('http://localhost:3000/testcollective/edit-twitter')
       .end();
