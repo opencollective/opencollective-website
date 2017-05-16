e63323215689324e9daef59d8a2d336948d22725
Pia Mancini <piamancini@gmail.com>
2017-05-16 11:37:11 -0400

    update faqs (#724)

--- frontend/src/containers/Faq.js
+++ frontend/src/containers/Faq.js
@@ -87,7 +87,7 @@ export class Faq extends Component {
               <li>Submit an invoice for your work. The invoice must include your current address and be made to Open Collective Inc (or to the host if the Collective is hosted by another organisation) with reference to the Collective name. The expense amount will be listed on the Collective page, but the invoice itself is only visible to the host and core contributors (not public).</li>
              </ul>
           </p>
-          <p>Expenses will be paid once a week on Fridays. Please contact <a href="mailto:support@opencollective.com">Support</a> if you have any questions.</p> 
+          <p>Only expenses approved by core contributors will be paid. To approve your team's expenses, use the links on the notification email or log in to your collective and click on the expense. Expenses are paid once a week on Fridays. Please contact <a href="mailto:support@opencollective.com">Support</a> if you have any questions.</p> 
           <p>Open Collective’s details:</p> 
           <p>
             OpenCollective, Inc.<br />
