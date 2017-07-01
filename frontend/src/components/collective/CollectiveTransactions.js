ad7752eb5e95644274f848825df087747d8eabbd
Xavier Damman <xdamman@gmail.com>
2017-07-01 19:21:29 -0400

    eslint

--- frontend/src/components/collective/CollectiveTransactions.js
+++ frontend/src/components/collective/CollectiveTransactions.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router';
 
 import CollectiveActivityItem from './CollectiveActivityItem';
 import TransactionEmptyState from '../TransactionEmptyState';
