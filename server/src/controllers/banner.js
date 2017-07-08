031bb8e2c69a21d63f85eed4d56391c6a01694b8
Xavier Damman <xdamman@gmail.com>
2017-07-08 19:44:05 +0200

    adapt the default height of the avatar based on number of avatars to show

--- server/src/controllers/banner.js
+++ server/src/controllers/banner.js
@@ -243,7 +243,6 @@ export default {
     const margin = req.query.margin ? Number(req.query.margin) : 5;
     const imageWidth = Number(req.query.width) || 0;
     const imageHeight = Number(req.query.height) || 0;
-    const avatarHeight = Number(req.query.avatarHeight) || 64;
     const count = Math.min(limit, users.length);
     const showBtn = (req.query.button === 'false') ? false : true;
 
@@ -252,6 +251,14 @@ export default {
         return (new Date(b.createdAt) - new Date(a.createdAt));
       });
     }
+
+    let defaultAvatarHeight = 64;
+    if ( users.length > 50)
+      defaultAvatarHeight = 48;
+    if ( users.length > 150)
+      defaultAvatarHeight = 24;
+
+    const avatarHeight = Number(req.query.avatarHeight) || defaultAvatarHeight;
 
     const params = (style === 'rounded') ? { query: `/c_thumb,g_face,h_${avatarHeight*2},r_max,w_${avatarHeight*2},bo_3px_solid_white/c_thumb,h_${avatarHeight*2},r_max,w_${avatarHeight*2},bo_2px_solid_rgb:66C71A/e_trim/f_auto/` } : { width: avatarHeight * 2, height: avatarHeight * 2};
 
