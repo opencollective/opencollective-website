bc0ff40b2c31c618f54001c278a308e36e238708
Aseem Sood <asood123@users.noreply.github.com>
2018-04-13 11:46:19 -0400

    Increase cache from 1 hour to 6 hours (#795)

--- server/src/controllers/homepage.js
+++ server/src/controllers/homepage.js
@@ -3,7 +3,7 @@ import api from '../lib/api';
 export default (req, res, next) => {
 
   api
-    .get(`/homepage`, { cache: 60 * 60 }) // cache for one hour
+    .get(`/homepage`, { cache: 60 * 60 * 6 }) // cache for six hours; increasing to see if it helps with downtime
     .then(homepage => {
       req.homepage = homepage;
     })
